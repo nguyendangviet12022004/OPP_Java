75c86b9138fa741c54c0a75458d3e74ab86f1402
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2023-08-14 22:01:02 +0700

    14-Aug-23

---
+++ b1/Main.java
@@ -0,0 +1,10 @@
+package b1;
+
+public class Main {
+    public static void main(String[] args) {
+        Staff s1 = new Staff("nam", 12, "nu", "Hoang Dieu", "an cut");
+        System.out.print(s1.toString());
+    }
+
+
+}
---
+++ b1/Officer.java
@@ -0,0 +1,16 @@
+package b1;
+
+public class Officer {
+    protected String name;
+    protected int age;
+    protected String gender;
+    protected String address;
+
+    public Officer(String name, int age, String gender, String address)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+}
