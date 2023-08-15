e333feebff85fea44ac350e08abe550b1f69c2c9
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2023-08-15 20:50:29 +0700

    Complete Exe1

--- b1/Main.java
+++ b1/Main.java
@@ -1,10 +1,84 @@
 package b1;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
+
     public static void main(String[] args) {
-        Staff s1 = new Staff("nam", 12, "nu", "Hoang Dieu", "an cut");
-        System.out.print(s1.toString());
+        // 1: insert, 2 : search, 3 : show, 4 : exit
+        Scanner scanner = new Scanner(System.in);
+        ManagerOfficer office = new ManagerOfficer();
+        boolean ok = true;
+        while(ok)
+        {
+            System.out.print("Command: ");
+            int command = scanner.nextInt();
+            switch(command)
+            {
+                case 1: 
+                {
+                    // a: E , b : S, c : W
+                    String type = scanner.next();
+                   
+                    String name = scanner.next();
+                    int age = scanner.nextInt();
+                    String gender = scanner.next();
+                    String address = scanner.next();
+                    switch(type)
+                    {
+                        case "a":
+                        {
+                            String branch = scanner.next();
+                            Engineer e = new Engineer(name, age, gender, address, branch);
+                            office.addOfficer(e);
+                            break;
+                        }
+                        case "b":
+                        {
+                            String task = scanner.next();
+                            Staff s = new Staff(name, age, gender, address, task);
+                            office.addOfficer(s);
+                            break;
+                        }
+                        case "c":
+                        {
+                            int level = scanner.nextInt();
+                            Worker w = new Worker(name, age, gender, address, level);
+                            office.addOfficer(w);
+                            break;
+                        }
+                        default:
+                        {
+                            System.out.println("Invalid");
+                        }
+                    }
+                    break;
+                    
+                }
+                case 2:
+                {
+                    String name = scanner.next();
+                    ArrayList<Officer> ans = office.searchByName(name);
+                    for(int i = 0 ;i < ans.size();i++) System.out.println(ans.get(i).toString());
+                    break;
+                }
+                case 3 :
+                {
+                    office.showListOfficers();
+                    break;
+                }       
+                case 4:
+                {
+                    scanner.close();
+                    ok = false;
+                    break;
+                }
+                default:
+                {
+                    System.out.println("Invalid");
+                }
+
+            }
+        }
+        
     }
-
-
 }
--- b1/ManagerOfficer.java
+++ b1/ManagerOfficer.java
@@ -1,5 +1,34 @@
 package b1;
-
+import java.util.ArrayList;
 public class ManagerOfficer {
-    
+    private ArrayList<Officer> officers;
+    public ManagerOfficer()
+    {
+        this.officers = new ArrayList<>();
+    }
+    public void addOfficer(Officer officer)
+    {
+        this.officers.add(officer);
+    }
+    public ArrayList<Officer> searchByName(String name)
+    {
+        ArrayList<Officer> ans = new ArrayList<>();
+        for(int i = 0 ;i < officers.size();i++)
+        {
+            if(officers.get(i).name.equals(name)) 
+            {
+                ans.add(officers.get(i));
+            }
+        }
+        return ans;
+    }
+    public void showListOfficers()
+    {
+        for(int i = 0;i < officers.size();i++)
+        {
+            System.out.println(officers.get(i).toString());
+        }
+    }
+
 }
+
